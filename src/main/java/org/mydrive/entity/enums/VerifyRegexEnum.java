3403ec47e8c8ee7f76d7fbd8df40e8bb89db2579
imelolee <meloligen@gmail.com>
2023-07-11 11:17:36 +0800

    Update AOP

---
+++ src/main/java/org/mydrive/entity/enums/VerifyRegexEnum.java
@@ -0,0 +1,32 @@
+package org.mydrive.entity.enums;
+
+public enum VerifyRegexEnum {
+    NO("", "不校验"),
+    EMAIL("^\\s*\\w+(?:\\.{0,1}[\\w-]+)*@[a-zA-Z0-9]+(?:[-.][a-zA-Z0-9]+)*\\.[a-zA-Z]+\\s*$", "邮箱"),
+    PASSWORD("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@#$%^&+=])(?!.*\\s).{8,18}$", "密码为数字，字母，特殊字符，长度为8-18位");
+
+
+    private String regex;
+    private String desc;
+
+    VerifyRegexEnum(String regex, String desc){
+        this.regex = regex;
+        this.desc = desc;
+    }
+
+    public String getRegex() {
+        return regex;
+    }
+
+    public void setRegex(String regex) {
+        this.regex = regex;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
