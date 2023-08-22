c7a411587b161285977062744dd2dbba505f70a9
imelolee <wataruri1129@gmail.com>
2023-08-22 22:51:12 +0800

    Update file category

--- src/main/java/org/mydrive/entity/enums/FileTypeEnum.java
+++ src/main/java/org/mydrive/entity/enums/FileTypeEnum.java
@@ -3,16 +3,19 @@ package org.mydrive.entity.enums;
 import org.apache.commons.lang3.ArrayUtils;
 
 public enum FileTypeEnum {
-    VIDEO(FileCategoryEnum.VIDEO, 1, new String[]{".mp4",".avi",".rmvb",".mkv",".mov"}, "视频"),
-    MUSIC(FileCategoryEnum.MUSIC, 2,new String[]{".mp3", ".flac",".acc",".wav",".wmv"}, "音乐"),
-    IMAGE(FileCategoryEnum.IMAGE, 3,new String[]{".jpg", ".jpeg",".png",".gif",".bmp",".psd","webp"}, "图片"),
-    PDF(FileCategoryEnum.DOC, 4,new String[]{".pdf"}, "pdf"),
-    WORD(FileCategoryEnum.DOC, 5,new String[]{".doc", ".docx"}, "word"),
-    EXCEL(FileCategoryEnum.DOC, 6,new String[]{".xls", ".xlsx"}, "excel"),
-    TXT(FileCategoryEnum.DOC, 7,new String[]{".txt"}, "txt文本"),
-    PROGRAME(FileCategoryEnum.OTHERS, 8,new String[]{".h", ".c",".cpp",".cc",".m",".java",".py",".html",".class",".css",".js",".ts",".sql",".xml",".json"}, "code"),
-    ZIP(FileCategoryEnum.OTHERS, 9,new String[]{".rar",".zip",".7z",".tar",".gz",".bz"}, "压缩包"),
-    OTHERS(FileCategoryEnum.OTHERS, 10,new String[]{}, "其他");
+    VIDEO(FileCategoryEnum.VIDEO, 1, new String[]{".mp4", ".avi", ".rmvb", ".mkv", ".mov"}, "video"),
+    MUSIC(FileCategoryEnum.MUSIC, 2, new String[]{".mp3", ".flac", ".acc", ".wav", ".wmv"}, "music"),
+    IMAGE(FileCategoryEnum.IMAGE, 3, new String[]{".jpg", ".jpeg", ".png", ".gif", ".bmp", ".psd", "webp"}, "image"),
+    PDF(FileCategoryEnum.DOC, 4, new String[]{".pdf"}, "pdf"),
+    WORD(FileCategoryEnum.DOC, 5, new String[]{".doc", ".docx"}, "word"),
+    EXCEL(FileCategoryEnum.DOC, 6, new String[]{".xls", ".xlsx"}, "excel"),
+    POWERPOINT(FileCategoryEnum.DOC, 7, new String[]{".ppt", ".pptx"}, "powerpoint"),
+    TXT(FileCategoryEnum.DOC, 8, new String[]{".txt"}, "text"),
+    PROGRAME(FileCategoryEnum.OTHERS, 9, new String[]{".h", ".c", ".cpp", ".cc", ".m", ".java", ".py", ".html", ".class", ".css", ".js", ".ts", ".sql", ".xml", ".json"}, "code"),
+    ZIP(FileCategoryEnum.OTHERS, 10, new String[]{".rar", ".zip", ".7z", ".tar", ".gz", ".bz"}, "zip"),
+    EXEC(FileCategoryEnum.OTHERS, 11, new String[]{".exe"}, "exe"),
+    OTHERS(FileCategoryEnum.OTHERS, 12, new String[]{}, "others");
+
 
     private FileCategoryEnum category;
     private Integer type;
@@ -42,18 +45,18 @@ public enum FileTypeEnum {
         return desc;
     }
 
-    public static FileTypeEnum getFileTypeBySuffix(String suffix){
-        for (FileTypeEnum item: FileTypeEnum.values()){
-            if (ArrayUtils.contains(item.getSuffix(), suffix)){
+    public static FileTypeEnum getFileTypeBySuffix(String suffix) {
+        for (FileTypeEnum item : FileTypeEnum.values()) {
+            if (ArrayUtils.contains(item.getSuffix(), suffix)) {
                 return item;
             }
         }
         return FileTypeEnum.OTHERS;
     }
 
-    public static FileTypeEnum getByType(Integer type){
+    public static FileTypeEnum getByType(Integer type) {
         for (FileTypeEnum item : FileTypeEnum.values()) {
-            if(item.getType().equals(type)){
+            if (item.getType().equals(type)) {
                 return item;
             }
         }
